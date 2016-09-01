e4c75470e77663dd28927b5e29ae8ad6f67685f9
Ryan McGlinn <ryanmcglinngdd@gmail.com>
2016-09-01 15:47:27 -0400

    Initial project commit to github

---
+++ src/imageResponses.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const spongegar = fs.readFileSync(`${__dirname} /../client/spongegar.png`);
+
+// may he rest in heaven...
+const harambe = fs.readFileSync(`${__dirname} /../client/harambe.png`);
+
+const getDankmemes = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(spongegar);
+  response.end();
+};
+
+const getRIP = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(harambe);
+  response.end();
+};
+
+module.exports.getDankmemes = getDankmemes;
+module.exports.getRIP = getRIP;
